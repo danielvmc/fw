22eecad3e8c2eb17506b4d713ed4002b45bb4f93
Huynh Minh Tu <hmtu.tl@gmail.com>
2014-12-10 19:32:50 +0700

    Fix some file

---
+++ src/storage/cache/ad1ec40894339cd3cbc1cda33e90b64b9d34b9e2.404.tpl.php
@@ -0,0 +1,95 @@
+<?php /*%%SmartyHeaderCode:86445148554883cd6bfeae4-99186860%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'ad1ec40894339cd3cbc1cda33e90b64b9d34b9e2' => 
+    array (
+      0 => '/home/vagrant/Code/www/fw/src/resources/templates/error/404.tpl',
+      1 => 1418214536,
+      2 => 'file',
+    ),
+    '942a1846f8922723cd5cc296646fb940caefa272' => 
+    array (
+      0 => '/home/vagrant/Code/www/fw/src/resources/templates/layout/header.tpl',
+      1 => 1418214536,
+      2 => 'file',
+    ),
+    'd663d3c69a475b1980e4f0ceb5e14fe2fd0c7256' => 
+    array (
+      0 => '/home/vagrant/Code/www/fw/src/resources/templates/layout/footer.tpl',
+      1 => 1418214536,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '86445148554883cd6bfeae4-99186860',
+  'variables' => 
+  array (
+    'errors' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_54883cd700dfc5_95466800',
+  'cache_lifetime' => 3600,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54883cd700dfc5_95466800')) {function content_54883cd700dfc5_95466800($_smarty_tpl) {?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>404</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<h1>404</h1>
+
+No routes found.
+
+<hr />
+
+<h3>The page you were looking for could not be found</h3>
+<p>This could be the result of the page being removed, the name being changed or the page being temporarily unavailable</p>
+<h3>Troubleshooting</h3>
+
+<ul>
+  <li>If you spelled the URL manually, double check the spelling</li>
+  <li>Go to our website's home page, and navigate to the content in question</li>
+</ul>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html><?php }} ?>
---
+++ src/storage/templates_c/348827592aeec1868233ae26bbfa25f3c1eaae20.file.index.tpl.cache.php
@@ -0,0 +1,36 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2014-12-10 12:31:24
+         compiled from "/home/vagrant/Code/www/fw/src/resources/templates/welcome/index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:88396573654883d1ce6c6a1-40864352%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '348827592aeec1868233ae26bbfa25f3c1eaae20' => 
+    array (
+      0 => '/home/vagrant/Code/www/fw/src/resources/templates/welcome/index.tpl',
+      1 => 1418214536,
+      2 => 'file',
+    ),
+    'a0ad89264fba5c4b57e75d2b10789947e0b514dc' => 
+    array (
+      0 => '/home/vagrant/Code/www/fw/src/resources/templates/layout/base.tpl',
+      1 => 1418214536,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '88396573654883d1ce6c6a1-40864352',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_54883d1d094248_39449569',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54883d1d094248_39449569')) {function content_54883d1d094248_39449569($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ('layout/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 9999, null, array(), 0);?>
+
+
+
+Welcome to the simple php framework
+
+
+<?php echo $_smarty_tpl->getSubTemplate ('layout/footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 9999, null, array(), 0);?>
+<?php }} ?>
